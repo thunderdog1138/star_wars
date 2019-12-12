df93b4518d73b055c6c520653fa2a10a56240193
thunderdog1138 <58536412+thunderdog1138@users.noreply.github.com>
2019-12-12 18:24:19 -0500

    Update loot.lua

--- mods/dungeon_loot/loot.lua
+++ mods/dungeon_loot/loot.lua
@@ -18,7 +18,7 @@ dungeon_loot.registered_loot = {
 	-- farming / consumable
 	{name = "farming:string", chance = 0.5, count = {1, 8}},
 	{name = "farming:wheat", chance = 0.5, count = {2, 5}},
-	{name = "default:apple", chance = 0.4, count = {1, 4}},
+	{name = "default:joganfruit", chance = 0.4, count = {1, 4}},
 	{name = "farming:seed_cotton", chance = 0.4, count = {1, 4},
 		types = {"normal"}},
 	{name = "default:cactus", chance = 0.4, count = {1, 4},
@@ -27,13 +27,13 @@ dungeon_loot.registered_loot = {
 	-- minerals
 	{name = "default:coal_lump", chance = 0.9, count = {1, 12}},
 	{name = "default:gold_ingot", chance = 0.5},
-	{name = "default:steel_ingot", chance = 0.4, count = {1, 6}},
+	{name = "default:durasteel_ingot", chance = 0.4, count = {1, 6}},
 	{name = "default:mese_crystal", chance = 0.1, count = {2, 3}},
 
 	-- tools
 	{name = "default:sword_wood", chance = 0.6},
 	{name = "default:pick_stone", chance = 0.3},
-	{name = "default:axe_diamond", chance = 0.05},
+	{name = "default:axe_beskar", chance = 0.05},
 
 	-- natural materials
 	{name = "default:sand", chance = 0.8, count = {4, 32}, y = {-64, 32768},
