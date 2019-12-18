41b243a6a176239b6e090427e3bced43318a9c9c
thunderdog1138 <58536412+thunderdog1138@users.noreply.github.com>
2019-12-18 11:08:29 -0500

    Update init.lua

--- mods/mobs_monster/init.lua
+++ mods/mobs_monster/init.lua
@@ -18,15 +18,7 @@ mobs.intllib = S
 
 -- Monsters
 
-dofile(path .. "/dirt_monster.lua") -- PilzAdam
-dofile(path .. "/dungeon_master.lua")
-dofile(path .. "/oerkki.lua")
-dofile(path .. "/sand_monster.lua")
-dofile(path .. "/stone_monster.lua")
-dofile(path .. "/tree_monster.lua")
-dofile(path .. "/lava_flan.lua") -- Zeg9
-dofile(path .. "/mese_monster.lua")
-dofile(path .. "/spider.lua") -- AspireMint
+dofile(path .. "/tusken_raider.lua")
 
 dofile(path .. "/lucky_block.lua")
 
