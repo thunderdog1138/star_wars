0760ad82dd8219b88d33286d733cb6510404d14b
thunderdog1138 <thunderdog1138@noreply.git.minetest.land>
2020-05-07 22:38:51 +0000

    Update 'mods/mobs_animal/init.lua'

--- mods/mobs_animal/init.lua
+++ mods/mobs_animal/init.lua
@@ -15,9 +15,7 @@ dofile(path .. "/sheep.lua") -- PilzAdam
 dofile(path .. "/warthog.lua") -- KrupnoPavel
 dofile(path .. "/bee.lua") -- KrupnoPavel
 dofile(path .. "/bunny.lua") -- ExeterDad
-dofile(path .. "/kitten.lua") -- Jordach/BFD
-dofile(path .. "/penguin.lua") -- D00Med
-dofile(path .. "/panda.lua") -- AspireMint
+dofile(path .. "/tooka.lua") -- Jordach/BFD
 
 dofile(path .. "/lucky_block.lua")
 
