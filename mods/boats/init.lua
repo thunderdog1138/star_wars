b78bae063cf3bc2222d2d78d7d928883101fcaf8
thunderdog1138 <thunderdog1138@noreply.git.minetest.land>
2020-05-17 20:05:40 +0000

    Update 'mods/boats/init.lua'

--- mods/boats/init.lua
+++ mods/boats/init.lua
@@ -36,7 +36,7 @@ local boat = {
 		collisionbox = {-0.5, -0.35, -0.5, 0.5, 0.3, 0.5},
 		visual = "mesh",
 		mesh = "boats_boat.obj",
-		textures = {"default_wood.png"},
+		textures = {"jogan_wood.png"},
 	},
 
 	driver = nil,
@@ -238,8 +238,8 @@ minetest.register_entity("boats:boat", boat)
 
 minetest.register_craftitem("boats:boat", {
 	description = S("Boat"),
-	inventory_image = "boats_inventory.png",
-	wield_image = "boats_wield.png",
+	inventory_image = "inventory.png",
+	wield_image = "wield.png",
 	wield_scale = {x = 2, y = 2, z = 1},
 	liquids_pointable = true,
 	groups = {flammable = 2},
@@ -282,8 +282,8 @@ minetest.register_craft({
 	output = "boats:boat",
 	recipe = {
 		{"",           "",           ""          },
-		{"group:wood", "",           "group:wood"},
-		{"group:wood", "group:wood", "group:wood"},
+		{"group:soft_wood", "",           "group:soft_wood"},
+		{"group:soft_wood", "group:soft_wood", "group:soft_wood"},
 	},
 })
 
