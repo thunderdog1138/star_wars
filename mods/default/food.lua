ca4b48930052b9ae5852649e9c326926320c8529
thunderdog1138 <thunderdog1138@noreply.git.minetest.land>
2020-05-17 20:52:00 +0000

    Add 'mods/default/food.lua'

---
+++ mods/default/food.lua
@@ -0,0 +1,22 @@
+-- support for MT game translation.
+local S = default.get_translator
+
+-- Jogan Fruit
+minetest.register_node("default:jogan_fruit", {
+	description = S("Jogan Fruit"),
+	drawtype = "plantlike",
+	tiles = {"jogan_fruit.png"},
+	inventory_image = "jogan_fruit.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	is_ground_content = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
+	},
+	groups = {fleshy = 3, dig_immediate = 3, flammable = 2,
+		leafdecay = 3, leafdecay_drop = 1, food_apple = 1},
+	on_use = minetest.item_eat(2),
+	sounds = default.node_sound_leaves_defaults(),
+})
