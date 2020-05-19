8671663b61af4eee27b5649dc86228ed5e004ea7
thunderdog1138 <thunderdog1138@noreply.git.minetest.land>
2020-05-19 16:57:45 +0000

    Update 'mods/ethereal/schematics/jogan_tree.lua'

--- mods/ethereal/schematics/jogan_tree.lua
+++ mods/ethereal/schematics/jogan_tree.lua
@@ -1,11 +1,11 @@
 
 local _ = {name = "air", param1 = 0}
-local L = {name = "ethereal:orange_leaves", param1 = 255}
-local l = {name = "ethereal:orange_leaves", param1 = 200}
-local T = {name = "default:tree", param1 = 255}
-local o = {name = "ethereal:orange", param1 = 200}
+local L = {name = "ethereal:jogan_leaves", param1 = 255}
+local l = {name = "ethereal:jogan_leaves", param1 = 200}
+local T = {name = "ethereal:jogan_trunk", param1 = 255}
+local o = {name = "ethereal:jogan_fruit", param1 = 200}
 
-ethereal.orangetree = {
+ethereal.jogan_tree = {
 
 	size = {x = 3, y = 6, z = 3},
 
