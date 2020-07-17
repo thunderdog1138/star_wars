74d0bad2ba0aaf6dfc1ab48328958eb2708563c0
thunderdog1138 <thunderdog1138@noreply.git.minetest.land>
2020-07-17 14:20:50 +0000

    Update 'mods/farming/crops/rishi_corn.lua'

--- mods/farming/crops/rishi_corn.lua
+++ mods/farming/crops/rishi_corn.lua
@@ -6,21 +6,21 @@
 
 local S = farming.intllib
 
--- corn
-minetest.register_craftitem("farming:corn", {
-	description = S("Corn"),
-	inventory_image = "farming_corn.png",
+-- rishi corn
+minetest.register_craftitem("farming:rishi_corn", {
+	description = S("Rishi Corn"),
+	inventory_image = "farming_rishi_corn.png",
 	groups = {seed = 2, food_corn = 1, flammable = 2},
 	on_place = function(itemstack, placer, pointed_thing)
-		return farming.place_seed(itemstack, placer, pointed_thing, "farming:corn_1")
+		return farming.place_seed(itemstack, placer, pointed_thing, "farming:rishi_corn_1")
 	end,
 	on_use = minetest.item_eat(3)
 })
 
--- corn on the cob (texture by TenPlus1)
-minetest.register_craftitem("farming:corn_cob", {
-	description = S("Corn on the Cob"),
-	inventory_image = "farming_corn_cob.png",
+-- rishi corn on the cob (texture by TenPlus1)
+minetest.register_craftitem("farming:rishi_corn_cob", {
+	description = S("Rishi Corn on the Cob"),
+	inventory_image = "farming_rishi_corn_cob.png",
 	groups = {food_corn_cooked = 1, flammable = 2},
 	on_use = minetest.item_eat(5)
 })
@@ -28,8 +28,8 @@ minetest.register_craftitem("farming:corn_cob", {
 minetest.register_craft({
 	type = "cooking",
 	cooktime = 10,
-	output = "farming:corn_cob",
-	recipe = "group:food_corn"
+	output = "farming:rishi_corn_cob",
+	recipe = "farming:rishi_corn"
 })
 
 -- cornstarch
@@ -85,10 +85,10 @@ minetest.register_craft({
 	replacements = {{ "farming:bottle_ethanol", "vessels:glass_bottle"}}
 })
 
--- corn definition
+-- rishi corn definition
 local def = {
 	drawtype = "plantlike",
-	tiles = {"farming_corn_1.png"},
+	tiles = {"farming_rishi_corn_1.png"},
 	paramtype = "light",
 	sunlight_propagates = true,
 	walkable = false,
@@ -103,56 +103,56 @@ local def = {
 }
 
 -- stage 1
-minetest.register_node("farming:corn_1", table.copy(def))
+minetest.register_node("farming:rishi_corn_1", table.copy(def))
 
 -- stage 2
-def.tiles = {"farming_corn_2.png"}
-minetest.register_node("farming:corn_2", table.copy(def))
+def.tiles = {"farming_rishi_corn_2.png"}
+minetest.register_node("farming:rishi_corn_2", table.copy(def))
 
 -- stage 3
-def.tiles = {"farming_corn_3.png"}
-minetest.register_node("farming:corn_3", table.copy(def))
+def.tiles = {"farming_rishi_corn_3.png"}
+minetest.register_node("farming:rishi_corn_3", table.copy(def))
 
 -- stage 4
-def.tiles = {"farming_corn_4.png"}
-minetest.register_node("farming:corn_4", table.copy(def))
+def.tiles = {"farming_rishi_corn_4.png"}
+minetest.register_node("farming:rishi_corn_4", table.copy(def))
 
 -- stage 5
-def.tiles = {"farming_corn_5.png"}
-minetest.register_node("farming:corn_5", table.copy(def))
+def.tiles = {"farming_rishi_corn_5.png"}
+minetest.register_node("farming:rishi_corn_5", table.copy(def))
 
 -- stage 6
-def.tiles = {"farming_corn_6.png"}
+def.tiles = {"farming_rishi_corn_6.png"}
 def.visual_scale = 1.9
-minetest.register_node("farming:corn_6", table.copy(def))
+minetest.register_node("farming:rishi_corn_6", table.copy(def))
 
 -- stage 7
-def.tiles = {"farming_corn_7.png"}
+def.tiles = {"farming_rishi_corn_7.png"}
 def.drop = {
 	items = {
-		{items = {"farming:corn"}, rarity = 1},
-		{items = {"farming:corn"}, rarity = 2},
-		{items = {"farming:corn"}, rarity = 3}
+		{items = {"farming:rishi_corn"}, rarity = 1},
+		{items = {"farming:rishi_corn"}, rarity = 2},
+		{items = {"farming:rishi_corn"}, rarity = 3}
 	}
 }
-minetest.register_node("farming:corn_7", table.copy(def))
+minetest.register_node("farming:rishi_corn_7", table.copy(def))
 
 -- stage 8 (final)
-def.tiles = {"farming_corn_8.png"}
+def.tiles = {"farming_rishi_corn_8.png"}
 def.groups.growing = nil
 def.drop = {
 	items = {
-		{items = {"farming:corn 2"}, rarity = 1},
-		{items = {"farming:corn 2"}, rarity = 2},
-		{items = {"farming:corn 2"}, rarity = 2}
+		{items = {"farming:rishi_corn 2"}, rarity = 1},
+		{items = {"farming:rishi_corn 2"}, rarity = 2},
+		{items = {"farming:rishi_corn 2"}, rarity = 2}
 	}
 }
-minetest.register_node("farming:corn_8", table.copy(def))
+minetest.register_node("farming:rishi_corn_8", table.copy(def))
 
 -- add to registered_plants
-farming.registered_plants["farming:corn"] = {
-	crop = "farming:corn",
-	seed = "farming:corn",
+farming.registered_plants["farming:rishi_corn"] = {
+	crop = "farming:rishi_corn",
+	seed = "farming:rishi_corn",
 	minlight = 13,
 	maxlight = 15,
 	steps = 8
