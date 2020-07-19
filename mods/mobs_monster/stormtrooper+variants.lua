f8c085b2d1fa5bc4b2fc31b1838c883026a92cc3
thunderdog1138 <thunderdog1138@noreply.git.minetest.land>
2020-07-19 15:28:50 +0000

    Update 'mods/mobs_monster/stormtrooper+variants.lua'

--- mods/mobs_monster/stormtrooper+variants.lua
+++ mods/mobs_monster/stormtrooper+variants.lua
@@ -1,7 +1,7 @@
 --Stormtrooper
 mobs:register_mob("mobs_monster:stormtrooper", {
 	type = "npc",
-	passive = true,
+	passive = false,
 	attack_monsters = false,
 	attack_npcs = false,
 	attack_players = true,
