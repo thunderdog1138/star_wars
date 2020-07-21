d6b445870426bf4c3a1deb044642fc9110aa2421
thunderdog1138 <thunderdog1138@noreply.git.minetest.land>
2020-07-21 22:01:25 +0000

    Update 'mods/adv_lightsabers/craftitems.lua'

--- mods/adv_lightsabers/craftitems.lua
+++ mods/adv_lightsabers/craftitems.lua
@@ -9,12 +9,12 @@ local hilts={"single","cross","double"}
 
 minetest.register_craftitem("adv_lightsabers:lightsaber_internals", { -- Lightsaber Internals
     description = "Lightsaber Internals",
-    inventory_image = "adv_lightsabers_lightsaber_internals.png"
+    inventory_image = "lightsaber_internals.png"
 })
 
 minetest.register_craftitem("adv_lightsabers:kyber_crystal", { -- Uncolored Lightsaber Crystal
     description = "Kyber Crystal",
-	inventory_image = "adv_lightsabers_kyber_crystal.png",
+	inventory_image = "kyber_crystal.png",
 	stack_max = 1,
 })
 
@@ -22,12 +22,12 @@ for _,color in ipairs(colors) do
     for _,hilt in ipairs(hilts) do
 minetest.register_craftitem("adv_lightsabers:kyber_crystal_"..color, { -- Colored Kyber Crystal
     description = "Kyber Crystal",
-    inventory_image = "adv_lightsabers_kyber_crystal_"..color..".png"
+    inventory_image = "kyber_crystal_"..color..".png"
 })
 
 minetest.register_craftitem("adv_lightsabers:lightsaber_hilt_"..hilt, { -- Lightsaber Hilt
     description = "Lightsaber Hilt",
-    inventory_image = "adv_lightsabers_hilt_"..hilt.."_inv.png"
+    inventory_image = "hilt_"..hilt.."_inv.png"
 })
 
 minetest.register_craft({ -- Lightsaber Crafting Recipe
