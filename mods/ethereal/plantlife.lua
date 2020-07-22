f9d94eeb79efc1fdfb6821680424ddb0940e5159
thunderdog1138 <thunderdog1138@noreply.git.minetest.land>
2020-07-22 19:18:16 +0000

    Update 'mods/ethereal/plantlife.lua'

--- mods/ethereal/plantlife.lua
+++ mods/ethereal/plantlife.lua
@@ -30,50 +30,14 @@ minetest.register_craft({
 	}
 })
 
--- Fern (boston)
-minetest.register_node("ethereal:fern", {
-	description = S("Fern"),
-	drawtype = "plantlike",
-	visual_scale = 1.4,
-	tiles = {"fern.png"},
-	inventory_image = "fern.png",
-	wield_image = "fern.png",
-	paramtype = "light",
-	sunlight_propagates = true,
-	waving = 1,
-	walkable = false,
-	buildable_to = true,
-	drop = {
-		max_items = 1,
-		items = {
-			{items = {"ethereal:fern_tubers"}, rarity = 6},
-			{items = {"ethereal:fern"}}
-		}
-	},
-	groups = {snappy = 3, flora = 1, attached_node = 1, flammable = 2},
-	sounds = default.node_sound_leaves_defaults(),
-	selection_box = {
-		type = "fixed",
-		fixed = {-5 / 16, -0.5, -5 / 16, 5 / 16, 0.67, 5 / 16},
-	},
-})
-
--- Boston Ferns sometimes drop edible Tubers (heals 1/2 heart when eaten)
-minetest.register_craftitem("ethereal:fern_tubers", {
-	description = S("Fern Tubers"),
-	inventory_image = "fern_tubers.png",
-	groups = {food_tuber = 1, flammable = 2},
-	on_use = minetest.item_eat(1),
-})
-
 -- Grey Shrub (not Flammable - too cold to burn)
 minetest.register_node("ethereal:snowygrass", {
 	description = S("Snowy Grass"),
 	drawtype = "plantlike",
 	visual_scale = 0.9,
-	tiles = {"ethereal_snowygrass.png"},
-	inventory_image = "ethereal_snowygrass.png",
-	wield_image = "ethereal_snowygrass.png",
+	tiles = {"snowygrass.png"},
+	inventory_image = "snowygrass.png",
+	wield_image = "snowygrass.png",
 	paramtype = "light",
 	sunlight_propagates = true,
 	waving = 1,
