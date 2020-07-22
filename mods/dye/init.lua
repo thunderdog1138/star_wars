02e29789b82a5c5cc2a640974be119e1a07c142b
thunderdog1138 <thunderdog1138@noreply.git.minetest.land>
2020-07-22 15:26:37 +0000

    Update 'mods/dye/init.lua'

--- mods/dye/init.lua
+++ mods/dye/init.lua
@@ -56,15 +56,6 @@ minetest.register_craft({
 	},
 })
 
--- Manually add blueberries->violet dye
-
-minetest.register_craft({
-	output = "dye:violet 2",
-	recipe = {
-		{"default:blueberries"}
-	},
-})
-
 -- Mix recipes
 
 local dye_recipes = {
