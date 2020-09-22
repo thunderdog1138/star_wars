5b876085bb3f5aa474ed276bb1f516ed18b1d92d
thunderdog1138 <thunderdog1138@noreply.git.minetest.land>
2020-09-22 19:23:47 +0000

    Upload files to 'mods/gunslinger'

---
+++ mods/gunslinger/guns.lua
@@ -0,0 +1,17 @@
+gunslinger.register_gun("gunslinger:cheetah", {
+	itemdef = {
+		description = "Cheetah (Assault Rifle)",
+		inventory_image = "gunslinger_cheetah.png",
+		wield_image = "gunslinger_cheetah.png^[transformFXR300",
+		wield_scale = {x = 3, y = 3, z = 1}
+	},
+
+	mode = "automatic",
+	dmg_mult = 2,
+	recoil_mult = 5,
+	fire_rate = 8,
+	clip_size = 30,
+	range = 80
+})
+
+minetest.register_alias("cheetah", "gunslinger:cheetah")
