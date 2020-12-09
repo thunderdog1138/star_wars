bda8d88f6cc849b4cc45423cde294e4c352c2bfd
thunderdog1138 <thunderdog1138@noreply.git.minetest.land>
2020-12-09 20:31:57 +0000

    Update 'mods/mobs_animal/init.lua'

--- mods/mobs_animal/init.lua
+++ mods/mobs_animal/init.lua
@@ -20,15 +20,15 @@ end
 
 -- Animals
 dofile(path .. "chicken.lua") -- JKmurray
-dofile(path .. "cow.lua") -- KrupnoPavel
-dofile(path .. "rat.lua") -- PilzAdam
-dofile(path .. "sheep.lua") -- PilzAdam
-dofile(path .. "warthog.lua") -- KrupnoPavel
+--dofile(path .. "cow.lua") -- KrupnoPavel
+--dofile(path .. "rat.lua") -- PilzAdam
+--dofile(path .. "sheep.lua") -- PilzAdam
+--dofile(path .. "warthog.lua") -- KrupnoPavel
 dofile(path .. "bee.lua") -- KrupnoPavel
-dofile(path .. "bunny.lua") -- ExeterDad
-dofile(path .. "kitten.lua") -- Jordach/BFD
-dofile(path .. "penguin.lua") -- D00Med
-dofile(path .. "panda.lua") -- AspireMint
+dofile(path .. "rabbit.lua") -- ExeterDad
+--dofile(path .. "kitten.lua") -- Jordach/BFD
+--dofile(path .. "penguin.lua") -- D00Med
+--dofile(path .. "panda.lua") -- AspireMint
 
 
 -- Load custom spawning
